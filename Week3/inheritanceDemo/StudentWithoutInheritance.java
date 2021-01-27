6379ed3f8e22c05c52af9790c48bb5c9ac434550
rainsharmin <rain.sharmin@gmail.com>
2021-01-26 23:38:52 -0500

    wk3

---
+++ Week3/inheritanceDemo/StudentWithoutInheritance.java
@@ -0,0 +1,41 @@
+package inheritanceDemo;
+
+/**
+ * Without using inheritance, the implementation of this student
+ * class duplicates part of the implementation of the Person class. 
+ * Not elegant.
+ * 
+ * Exercise: In this package, in a file named Student.java, make a Student class that
+ * inherits from Person, and avoids repeating any code from the superclass
+ * (Hint: The first line in the file, after package inheritanceDemo, will be
+ * "public class Student extends Person")
+ */
+public class StudentWithoutInheritance {
+	
+	private String name;
+	private int age;
+	private String studentNumber;
+	
+	public StudentWithoutInheritance(String name, int age, String stuNumber) {
+		this.name = name;
+		this.age = age;
+		this.studentNumber = stuNumber;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public int getAge() {
+		return this.age;
+	}
+	
+	public String getStudentNumber() {
+		return this.studentNumber;
+	}
+
+	public String hello() {
+		return "Hello, I am " + this.getName() + " and my student number is " + this.getStudentNumber();
+	}
+
+}
