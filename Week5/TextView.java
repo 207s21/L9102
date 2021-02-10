8223d83d4abf7a0f06f4bd8c1fac5f9f7357dc1d
rainsharmin <rain.sharmin@gmail.com>
2021-02-10 15:10:49 -0500

    wk5

---
+++ Week5/TextView.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+/**
+ * Present a text view of Balloons
+ */
+public class TextView implements Observer {
+
+	@Override
+	public void update(Observable o, Object arg) {
+		System.out.println("Message: " + o.toString() + " has been " + arg);
+	}
+}
